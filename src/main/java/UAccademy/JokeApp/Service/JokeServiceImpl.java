b2f328fd6a9a2b41be9557a31507a0aee4431862
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-06 14:49:25 +0100

    View and controller

--- src/main/java/UAccademy/JokeApp/Service/JokeServiceImpl.java
+++ src/main/java/UAccademy/JokeApp/Service/JokeServiceImpl.java
@@ -6,9 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService{
 
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    }
+
     public ChuckNorrisQuotes chuckNorrisQuotes;
-
-
 
     public String getJoke(){
         return chuckNorrisQuotes.getRandomQuote();
